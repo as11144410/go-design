94e1e3c5d639bfecaec0a2ddef148b8376e099ad
as11144410 <11144410@qq.com>
2021-09-26 15:23:21 +0800

    Update product.go

--- product.go
+++ product.go
@@ -1,35 +1,33 @@
-package main
+package go_design
 
 type Product interface {
 	SetName(name string)
 	GetName() string
 }
 
-// 产品1
+// Product1 产品1
 type Product1 struct {
 	name string
 }
 
-func (p1 Product1) SetName(name string) {
+func (p1 *Product1) SetName(name string) {
 	p1.name = name
 }
 
-
-func (p1 Product1) GetName() string {
+func (p1 *Product1) GetName() string {
 	return "产品1的name为:" + p1.name
 }
 
-// 产品2
+// Product2 产品2
 type Product2 struct {
 	name string
 }
 
-func (p2 Product2) SetName(name string) {
+func (p2 *Product2) SetName(name string) {
 	p2.name = name
 }
 
-
-func (p2 Product2) GetName() string {
+func (p2 *Product2) GetName() string {
 	return "产品2的name为:" + p2.name
 }
 
@@ -37,16 +35,15 @@ func (p2 Product2) GetName() string {
 
 type productType int
 
-const(
+const (
 	p1 productType = iota
 	p2
 )
 
 type productFactory struct {
-
 }
 
-func (pf productFactory) Create(productType productType) Product {
+func (pf *productFactory) Create(productType productType) Product {
 	if productType == p1 {
 		return &Product1{}
 	}
