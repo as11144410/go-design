78ad16a685cf16b077ae89b311c8303059649e49
as11144410 <11144410@qq.com>
2021-09-26 15:13:17 +0800

    up

--- product.go
+++ product.go
@@ -1,1 +1,57 @@
 package main
+
+type Product interface {
+	SetName(name string)
+	GetName() string
+}
+
+// 产品1
+type Product1 struct {
+	name string
+}
+
+func (p1 Product1) SetName(name string) {
+	p1.name = name
+}
+
+
+func (p1 Product1) GetName() string {
+	return "产品1的name为:" + p1.name
+}
+
+// 产品2
+type Product2 struct {
+	name string
+}
+
+func (p2 Product2) SetName(name string) {
+	p2.name = name
+}
+
+
+func (p2 Product2) GetName() string {
+	return "产品2的name为:" + p2.name
+}
+
+// 实现简单工厂类
+
+type productType int
+
+const(
+	p1 productType = iota
+	p2
+)
+
+type productFactory struct {
+
+}
+
+func (pf productFactory) Create(productType productType) Product {
+	if productType == p1 {
+		return &Product1{}
+	}
+	if productType == p2 {
+		return &Product2{}
+	}
+	return nil
+}
